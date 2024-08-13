08e9ae317150f9403515b2bee90eab24c5aeae8d
Gugulothu Mukesh Varma <mukeshvarma.g@gmail.com>
2024-08-13 15:39:46 +0530

    Sonar and WebConfig

---
+++ src/main/java/com/javatpoint/WebConfig.java
@@ -0,0 +1,18 @@
+package com.javatpoint;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
